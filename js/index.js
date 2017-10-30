d9003973baf5259579f6995ba395a82bd4e7c24d
wish <253094267@qq.com>
2017-10-30 21:04:42 +0800

    第一天提交

--- js/index.js
+++ js/index.js
@@ -31,14 +31,17 @@ $("#Banner").mouseout(function(){
     $("#RightBtn").hide();
 })
 $(".bannerBtn").hover(function(){
-    $(".bannerBtn").css({backgroundColor:"#000"});
+    $(".bannerBtn").css({backgroundColor:"rgba(0,0,0,0.6)"});
 })
 $(".bannerBtn").mouseout(function(){
-    $(".bannerBtn").css({backgroundColor:"transparent"});
+    $(".bannerBtn").css({backgroundColor:""});
 })
 $("#LeftBtn").click(function(){
     clearInterval(timer);
     index--;
+    if(index<0){
+        index=5+index;
+    }
     $("#Banner #img").animate({"left":-index*x},300,function(){
         if(index>=5){
             $("#Banner #img").css({left:0})
@@ -53,7 +56,7 @@ $("#RightBtn").click(function(){
     clearInterval(timer);
     index++;
     $("#Banner #img").animate({"left":-index*x},300,function(){
-        if(index>=5){
+        if(index>=5||index<0){
             $("#Banner #img").css({left:0})
             index=0;
         }
@@ -65,6 +68,7 @@ $("#RightBtn").click(function(){
 
 /*商品获取并陈列*/
 $.get("http://h6.duchengjiu.top/shop/api_goods.php",{pagesize:18,page:3},function(obj){
+    var brandArr=["Burj Al-Arab","Rolex","Chanel","Tiffany","Louis Vuitton","Hennessy","Ralph Lauren"]
     var oUl=document.createElement("ul");
     $("#GoodShow").append(oUl);
     for(i=0;i<obj.data.length;i++){
@@ -105,6 +109,33 @@ $.get("http://h6.duchengjiu.top/shop/api_goods.php",{pagesize:18,page:3},functio
             $(oBox).mouseout(function(){
                 $(oBox).css({opacity:0});
             })
+
+            //加上商家信息，随机数。
+            var oBrand=document.createElement("div");
+            $(oBrand).addClass("who");
+            oLi.appendChild(oBrand);
+            var Bimg=document.createElement("img");
+            $(Bimg).addClass("Bimg");
+            var Bspan=document.createElement("span");
+            var z=parseInt(Math.random()*7);
+            Bspan.innerText=brandArr[z];
+            var x=parseInt(Math.random()*300+500);
+            var y=parseInt(Math.random()*300+300);
+            Bimg.src="http://imgs-qn.iliangcang.com/ware/brand/"+x+".jpg";
+            oBrand.appendChild(Bimg);
+            oBrand.appendChild(Bspan);
+
+            var oStar=document.createElement("div");
+            $(oStar).addClass("star")
+            var Simg=document.createElement("img");
+            var Stext=document.createElement("div");
+            Stext.innerText=y;
+            $(Simg).addClass("Simg");
+            Simg.src="http://imgs-qn.iliangcang.com/images/default/heart_gray20_18.png";
+            $(oStar).append(Simg);
+            $(oStar).append(Stext);            
+            oBrand.appendChild(oStar);
+
         })(i);
     }
 })
