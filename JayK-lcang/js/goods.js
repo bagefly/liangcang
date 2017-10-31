a087d1f486db069c1706041665606cde0c3b5a62
bagefly <huangxhe@126.com>
2017-10-31 17:08:57 +0800

    第二天白天：首页、商店、分享、商品详情布局

---
+++ JayK-lcang/js/goods.js
@@ -0,0 +1,5 @@
+
+var url = "http://h6.duchengjiu.top/shop/api_goods.php";
+$.get(url,{"goods_id":"245154"},function(str){
+    console.log(str);
+})
---
+++ JayK-lcang/readme.md
@@ -0,0 +1,8 @@
+
+# 1. 项目
+
+```css
+background-color:#5b799a!important;
+```
+# 右边三角
+    hover三角显示了价格,然后把鼠标移到价格下拉菜单,下拉菜单消失
